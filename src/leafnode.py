3cec72d343eac92de39a21d32fd2cba1ee8f2f8e
shade477 <sagardeyof477@gmail.com>
2024-11-02 00:22:45 +0530

    added eq function

--- src/leafnode.py
+++ src/leafnode.py
@@ -15,3 +15,6 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f'LeafNode({self.tag}, {self.value}, {self.props})'
+    
+    def __eq__(self, other):
+        return self.tag == other.tag and self.value == other.value and self.props == other.props
