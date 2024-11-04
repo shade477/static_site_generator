0aad9bd0eaed97880638922142bf376fa397a698
shade477 <sagardeyof477@gmail.com>
2024-11-04 22:06:45 +0530

    split base

--- src/splits.py
+++ src/splits.py
@@ -1,6 +1,12 @@
-from src.textnode import TextNode
+from src.textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-
     
+    for node in old_nodes:
+        
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        
