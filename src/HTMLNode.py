2cc85077057e5f198abfdba4a040593eb3759d94
shade477 <sagardeyof477@gmail.com>
2025-01-22 00:36:12 +0530

    added a eq function

--- src/HTMLNode.py
+++ src/HTMLNode.py
@@ -18,4 +18,11 @@ class HTMLNode:
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})'
 
+    def __eq__(self, node):
+        return (self.tag == node.tag and 
+                self.value == node.value and 
+                self.children == node.children and 
+                self.props == node.props
+        )
+
 
